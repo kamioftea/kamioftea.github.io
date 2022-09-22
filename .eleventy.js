660b84ce1a3d5ae11429c16e74dc503d860d490d
Jeff Horton <jeff@goblinoid.co.uk>
2022-09-23 00:31:13 +0100

    Crusade - sort units by codex order

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,44 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const feather = require('feather-icons');
 const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
+const dataSheets = require('./_data/dataSheets.json');
 
 function buildDeepGetter(path) {
     if(!path) return (obj) => obj
     const [key, ...rest] = path.split('.');
     return (obj) => buildDeepGetter(rest.join("."))(obj[key]);
+}
+
+class Comparator {
+    #fn
+
+    constructor(fn) {
+        this.#fn = this.#toComparator(fn)
+    }
+
+    #toComparator(fn) {
+        return (a, b) => {
+            const [resA, resB] = [fn(a), fn(b)];
+            if(typeof resA === 'number' && typeof resB === 'number') {
+                return resA - resB
+            }
+            return resA.toString().localeCompare(resB.toString())
+        }
+    }
+
+    thenComparing(fn) {
+        const prev = this.#fn;
+        const next = this.#toComparator(fn)
+        this.#fn = (a, b) => {
+            const prevRes = prev(a, b);
+            return prevRes || next(a, b)
+        }
+        return this
+    }
+
+    build() {
+        return this.#fn
+    }
 }
 
 module.exports = function(eleventyConfig) {
@@ -34,6 +67,13 @@ module.exports = function(eleventyConfig) {
         const lens = buildDeepGetter(path)
         return arr.map((obj) => lens(obj))
     })
+
+    eleventyConfig.addNunjucksFilter('sortUnits', (units) =>
+        [...units].sort(
+            new Comparator((unit) => [...Object.keys(dataSheets)].indexOf(unit.data.type))
+                .thenComparing((unit) => unit.data.name)
+                .build()
+        ))
 
     return {
         passthroughFileCopy: true,
